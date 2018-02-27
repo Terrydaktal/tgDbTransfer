ce42a93584605c458657773530f4cb6a46fd667b
Terrydaktal <9lewis9@gmail.com>
2018-02-27 10:35:18 +0000

    Update txtToDatabse.py

--- txtToDatabse.py
+++ txtToDatabse.py
@@ -2,16 +2,19 @@ import datetime
 import glob
 import os
 from transfer import insert, retrieveBlanks
+firstline = True
 
 def lineParser(line, message, state):
   space = 0
   comma = False
+  global firstline
+
+  if firstline:
+    times.append(1431905243)
+    firstline = False
+    
   if state == "new":
     for char in line:
-      tempStr = "" 
-      if not char == "," and not comma:
-        tempStr += char
-      else: comma = True
 
       if char.isspace():
         space += 1
@@ -26,8 +29,12 @@ def lineParser(line, message, state):
 
 def configureDirectories():
   directories = ["Photos\\", "Videos\\", "Audios\\"]
+  allFiles = []
   for i in directories:
-    files = filter(os.path.isfile, glob.glob(i + "*")).sort(key=lambda x: os.path.getmtime(x))
+    files1 = filter(os.path.isfile, glob.glob(i + "*"))
+    files = [i for i in files1]
+    files.sort(key=lambda x: os.path.getmtime(x))
+    files = [i for i in reversed(files)]
     allFiles.append(files)
                 
   return allFiles
@@ -39,64 +46,68 @@ def configureDirectories():
 def checkMedia(line, voice, photo, video, allFiles, mediaFiles):  
   if "[[Photo" in line:
     line.replace("[[Photo]]", allFiles[0][photo])
-    photo+=1
     mediaFiles.append(allFiles[0][photo])
-  elif "[[Voice Message" in line:
-    line.replace("[[Voice Message]]", allFiles[0][voice])
-    voice+=1
-    mediaFiles.append(allFiles[0][voice])
+    photo+=1
   elif "[[Video" in line:
-    line.replace("[[Video]]", allFiles[0][video])
-    mediaFiles.append(allFiles[0][video])
+    line.replace("[[Video]]", allFiles[1][video])
+    mediaFiles.append(allFiles[1][video])
     video+=1
+  elif "[[Voice Message" in line:
+    line.replace("[[Voice Message]]", allFiles[2][voice])
+    mediaFiles.append(allFiles[2][voice])
+    voice+=1
   else:
     mediaFiles.append(None)
-  return voice, photo, video, allFiles, mediaFiles
+  return line, voice, photo, video, allFiles, mediaFiles
                  
 if __name__ == '__main__':
   print("configuring...")
-  messages, sendIds, times, mediaFiles = ([],)*4
-  voice, photo, video = (0,)*3
+  messages, sendIDs, times, mediaFiles = [], [], [], []
+  voice, photo, video = 0, 0, 0
 
   print("configuring directories...")
   allFiles = configureDirectories()
   
   print("extracting backup...")               
-  with open('backup.txt', 'r') as backupFile:
-    for i, line in enumerate(backupFile):
-      finish = false
+  with open('backup.txt', 'r', encoding="utf8") as backupFile:
+    lines = backupFile.readlines()
+    for i, line in enumerate(lines):
+      finish = False
       if "Fedex Master" in line:
         sendIDs.append(54129829)
       else: sendIDs.append(67106936)
       
-      voice, photo, video, allFiles, mediaFiles = checkMedia(line, voice, photo, video, allFiles, mediaFiles)
+      line, voice, photo, video, allFiles, mediaFiles = checkMedia(line, voice, photo, video, allFiles, mediaFiles)
      
       message = lineParser(line, "", "new")
 
-    if backupFile[i+1][0].isdigit():
-      day, month, year = backupfile[i+1][0:10].split('.')
+    if i < len(lines) - 1 and lines[i+1][0].isdigit():
+      day, month, year = lines[i+1][0:10].split('.')
       while not finish:
         try:
           time = datetime(int(year),int(month),int(day).total_seconds() - datetime(1970,1,1)).total_seconds()
           times.append(time)
         except ValueError:
-          message+=""
-          lineParser(line, message, "continuation")
+          message+=" "
+          message = lineParser(line, message, "continuation")
         else: 
           messages.append(message)
-          finish = true
+          finish = True
           
   backupFile.close()
 
   print("building database...")   
   rows = []
-  for i in range(times.length()-1):
-    blankIDs = retrieveBlanks()
-    media_type = "photo" if ".png" in mediaFiles[i] or ".jpg" in mediaFiles[i] else "document"
-    record = (blankID[i], blankID[i], "message", "dialog", source_id, sendIDs[i], None, messages[i], 
+  blankIDPairs = retrieveBlanks("database.sqlite")
+
+  print(times)
+  for i in range(len(times) - 1):
+    media_type = "photo" if ".png" in str(mediaFiles[i]) or ".jpg" in str(mediaFiles[i]) else "document"
+    record = (blankIDPairs[0][i], blankIDPairs[1][i], "message", "dialog", "54129829", sendIDs[i], None, messages[i], 
               times[i], 1, media_type, mediaFiles[i], None, None, None, None, 53)
     rows.append(record)
     
   print("commiting changes...")   
   insert("database.sqlite", rows)
+  print("done!")
   
