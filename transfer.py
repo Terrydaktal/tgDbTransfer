3bf88ee92badfafc94678e7f2c6e1c88020918c1
Terrydaktal <9lewis9@gmail.com>
2018-02-27 10:36:16 +0000

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -5,7 +5,7 @@ import shutil
 
 def extract (friendDatabase, chatID, startMessageID, endMessageID):
     with conn:
-        conn = create_connection(friendDatabase)
+        conn = sqlite3.connect(friendDatabase)
         c = conn.cursor()
         c.execute('SELECT * FROM messages WHERE sourceID="{0}"'.format(chatID))
         rows = c.fetchall()
@@ -13,39 +13,45 @@ def extract (friendDatabase, chatID, startMessageID, endMessageID):
         conn.close()
     return rows
         
-def retrieveBlanks():
-    conn = create_connection(homeDatabase)
+def retrieveBlanks(homeDatabase):
+    conn = sqlite3.connect(homeDatabase)
     with conn:
         c = conn.cursor()
         c.execute('SELECT * FROM messages WHERE message_type = "empty_message"')
         blanks = c.fetchall()
-        blankIDs = [blanks[j][0] for j in range(blanks - 1)]
+        blankIDPairs = []
+        blankIDPairs.append([blanks[j][0] for j in range(len(blanks) - 1)])
+        blankIDPairs.append([blanks[j][1] for j in range(len(blanks) - 1)])
+        
     conn.close()
-    return blankIDs
+    return blankIDPairs
     
 def insert (homeDatabase, rows):
-    conn = create_connection(homeDatabase)
+    conn = sqlite3.connect(homeDatabase)
     with conn:
-        blankIDS = retrieveBlanks()
-        for i in range (rows.length()-1):
-            lst = list(rows[i])
-            lst[0] = blankIDs[i]
-            rows[i] = tuple(lst)
-            
-        for i in range (rows.length()-1): c.executemany("INSERT INTO messages('id', 'message_id', 'message_type',\
-                  'source_type', 'source_id', 'sender_id', 'fwd_from_id', 'text', 'time', 'has_media', 'media_type',\
-                  'media_file', 'media_size', 'media_json', 'marlup_json', 'data', 'api_layer') VALUES({0}, {1}, {2},\
-                  {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}, {16})".format(rows[i][0],
-                    rows[i][1], rows[i][2], rows[i][3], rows[i][4], rows[i][5], rows[i][6], rows[i][7], rows[i][8]
-                    rows[i][9], rows[i][10], rows[i][11], rows[i][12], rows[i][13], rows[i][14], rows[i][15], rows[i][16]))
+        c = conn.cursor()
+        blankIDPairs = retrieveBlanks(homeDatabase)
         
-        fileIDs = [rows[j][11] for j in range(rows.length() - 1)]
-        for i in fileIDs:
-            if not os.path.exists(homeFileDirectory + i):
-                shutil.copy(friendFileDirectory+i, homeFileDirectory+i)      
+      #  for i in range (len(rows) - 1):
+      #      lst = list(rows[i])
+      #      lst[0] = blankIDs[i]
+      #      rows[i] = tuple(lst)
+            
+        for i in range (len(rows) - 1):
+            c.execute('''REPLACE INTO messages('id', 'message_id', 'message_type',
+                                  'source_type', 'source_id', 'sender_id', 'fwd_from_id', 'text', 'time', 'has_media', 'media_type',
+                                  'media_file', 'media_size', 'media_json', 'markup_json', 'data', 'api_layer')
+                         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',\
+                                  (rows[i][0], rows[i][1], rows[i][2], rows[i][3], rows[i][4], rows[i][5], rows[i][6], rows[i][7], rows[i][8],
+                                   rows[i][9], rows[i][10], rows[i][11], rows[i][12], rows[i][13], rows[i][14], rows[i][15], rows[i][16]))
+            
+       # fileIDs = [rows[j][11] for j in range(len(rows) - 1)]
+       # for i in fileIDs:
+       #     if not os.path.exists(homeFileDirectory + i):
+       #         shutil.copy(friendFileDirectory+i, homeFileDirectory+i)      
 
-        conn.commit()        
-        conn.close()
+    conn.commit()        
+    conn.close()
         
 if __name__ == '__main__':
     try: 
